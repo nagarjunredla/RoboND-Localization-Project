803b9e000337fb39d61dd87bf00c5f2a0f21ee7d
Nagarjun Redla <nagarjunredla@gmail.com>
2018-10-30 21:29:05 -0700

    added rviz configuration

--- ShellScripts/launch.sh
+++ ShellScripts/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch udacity_bot udacity_world.launch " &
-sleep 5
+sleep 15
 xterm  -e "cd $(pwd)/../..; source devel/setup.bash ; roslaunch udacity_bot amcl.launch " &
 sleep 5
 xterm -e "cd $(pwd)/../..; source devel/setup.bash ; rosrun udacity_bot navigation_goal  " &
